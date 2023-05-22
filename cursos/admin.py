900f895b008d6d0218e44ae33f4fd397803b483f
Victor Ruan Diniz Pereira <50781733+ivictoruan@users.noreply.github.com>
2023-05-22 12:19:36 -0300

    FIX: removendo atributo incorreto adicionando o correto;

--- cursos/admin.py
+++ cursos/admin.py
@@ -5,10 +5,10 @@ from .models import Curso, Avaliacao
 
 @admin.register(Curso)
 class CursoAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'url', 'criacao', 'avaliacao', 'ativo')
+    list_display = ('titulo', 'url', 'criacao', 'atualizacao', 'ativo')
 
 
 @admin.register(Avaliacao)
 class AvaliacaoAdmin(admin.ModelAdmin):
     list_display = ('curso', 'nome', 'email', 'avaliacao', 'criacao', 'atualizacao', 'ativo')
-    
+
