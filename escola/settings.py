e6ff1117e306c4cea521446450154f1bb78d15ba
Victor Ruan Diniz Pereira <50781733+ivictoruan@users.noreply.github.com>
2023-05-22 19:58:22 -0300

    UPDATE: configurando drf, add app django_filters;

--- escola/settings.py
+++ escola/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'cursos',
+    'django_filters',
+    'rest_framework',
 
+    'cursos',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,13 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# DRF
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    )
+}
