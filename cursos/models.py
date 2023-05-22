f3142998e41f1906e816a1a251352ac8bcf8c5ac
Victor Ruan Diniz Pereira <50781733+ivictoruan@users.noreply.github.com>
2023-05-22 11:45:46 -0300

    FEAT: model Curso;

--- cursos/models.py
+++ cursos/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Base(models.Model):
+    criacao = models.DateTimeField(auto_now_add=True)
+    atualizacao = models.DateTimeField(auto_now=True)
+    ativo = models.BooleanField(default=True)
+
+    class meta:
+        abstratic = True
+
+class Curso(Base):
+    titulo = models.TextField(max_length=255)
+    url = models.URLField(unique=True)
+    
+    class Meta:
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
+    
+    def __str__(self):
+        return self.titulo
