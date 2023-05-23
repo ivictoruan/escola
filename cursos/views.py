67cffa8fd7ba98202ebf9c90499be2b14c875575
Victor Ruan Diniz Pereira <50781733+ivictoruan@users.noreply.github.com>
2023-05-23 11:21:48 -0300

    FEAT: APIView de Curso e Avaliação com funções get;

--- cursos/views.py
+++ cursos/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers  import CursoSerializer, AvaliacaoSerializer
 
-# Create your views here.
+from cursos.models import Curso, Avaliacao
+
+
+class CursoAPIView(APIView):
+    """
+    API DE CURSOS do canal ...
+    """
+    def get(self, request):
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+        return Response(serializer.data)
+
+
+class AvaliacaoAPIView(APIView):
+    """
+    API DE AVALIAÇÕES do canal ...
+    """
+    def get(self, request):
+        avaliacoes = Avaliacao.objects.all()
+        serializer = AvaliacaoSerializer(avaliacoes, many=True)
+        return Response(serializer.data)
