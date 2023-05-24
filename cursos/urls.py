a8af478fc5455602c984362ac9c8e4b18daa0f8d
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2023-05-24 17:56:39 -0300

    UPDATE: add rota p/ acesso às avaliações de cursos

--- cursos/urls.py
+++ cursos/urls.py
@@ -3,8 +3,15 @@ from django.urls import path
 from .views import CursoAPIView, CursosAPIView, AvaliacaoAPIView, AvaliacoesAPIView
 
 urlpatterns = [
-    path('cursos/', CursosAPIView.as_view(), name='cursos'),
+    path('cursos/', CursosAPIView.as_view(), name='cursos',),
+    path('cursos/<int:pk>/', CursoAPIView.as_view(), name='curso',),
+
+    path('cursos/<int:curso_pk>/avaliacoes/<int:avaliacao_pk>/',
+         AvaliacaoAPIView.as_view(), name='curso_avaliacao',),
+
+    path('cursos/<int:curso_pk>/avaliacoes/',
+         AvaliacoesAPIView.as_view(), name='curso_avaliacoes'),
+
     path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes'),
-    path('cursos/<int:pk>/', CursoAPIView.as_view(), name='curso'),
-    path('avaliacoes/<int:pk>/', AvaliacaoAPIView.as_view(), name='avalicao'),
+    path('avaliacoes/<int:avaliacao_pk>/', AvaliacaoAPIView.as_view(), name='avalicao'),
 ]
