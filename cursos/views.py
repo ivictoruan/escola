94ff88ea8b8e90820eb56158709b450eaf9a1c6d
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2023-05-24 17:57:41 -0300

    UPDATE: customizando endpoints para acesso a avaliações de um curso e uma avaliação específica;

--- cursos/views.py
+++ cursos/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
@@ -9,16 +10,32 @@ class CursosAPIView(generics.ListCreateAPIView):
     serializer_class = CursoSerializer
 
 
-class AvaliacoesAPIView(generics.ListCreateAPIView):
-    queryset = Avaliacao.objects.all()
-    serializer_class = AvaliacaoSerializer
-
-
 class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
 
+class AvaliacoesAPIView(generics.ListCreateAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
+
+    def get_queryset(self):
+        # print(self.queryset)
+        if self.kwargs.get('curso_pk'):  # se tiver "curso_pk" na URI
+            # avaliacoes relacionadas ao curso
+            return self.queryset.filter(curso_id=self.kwargs.get('curso_pk'))
+        else:
+            return self.queryset.all()
+
+
 class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
+
+    def get_object(self):
+        if self.kwargs.get('curso_pk'):
+            return get_object_or_404(  # RETORNA AVALIAÇÃO (SE HOUVER) DE UM CURSO
+                self.get_queryset(),
+                curso_id=self.kwargs.get('curso_pk'),
+                pk=self.kwargs.get('avaliacao_pk'))
+        return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('avaliacao_pk'))
